06195de7c1f88ee0f6e4addc28db0e794658b4c3
hijoo <fluffybulb@gmail.com>
2025-03-21 18:05:47 +0900

    heaven index done

--- js/heaven-main.js
+++ js/heaven-main.js
@@ -1,31 +1,10 @@
 $(function () {
-    const $title = $(".title-wrap strong");
+    const $title = $(".grid-wrap h2");
+    const $window = $(window);
 
     console.log($title);
-
     $title.on("click", function () {
-        // transition 속성 임시 제거
-        $title.css("transition", "none");
-        $(this).css("transition", "none");
-
-        $title.fadeIn(500, "linear");
-
-        // jQuery 애니메이션 실행
-        $(this).fadeOut(500, "linear", function () {
-            // 애니메이션이 끝난 후 transition 복원
-            $title.css("transition", "");
-            $(this).css("transition", "");
-        });
-    });
-
-    $(document).on("click", function (e) {
-        if (!$(e.target).closest(".title-wrap strong").length) {
-            $title.css("transition", "none");
-
-            $title.fadeIn(500, "linear", function () {
-                // 애니메이션이 끝난 후 transition 복원
-                $title.css("transition", "");
-            });
-        }
+        gsap.to($title, { opacity: 1, userSelect: "initial", pointerEvents: "initial", duration: 0.2 });
+        gsap.to($(this), { opacity: 0, userSelect: "none", pointerEvents: "none", duration: 0.3 });
     });
 });
